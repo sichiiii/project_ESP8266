d7c9e4f4652fe26886514f9f60b0ef184bc2def6
jabka <jabka@jabka.jabka>
2021-10-10 09:24:45 +0300

    status page backend

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ directory=Path(__file__).parent.parent.absolute()
 @esp8266.get("/")
 async def index(request: Request):
     try:
-        print(request.client.host)  # IP ---> Database
-        return {'ports':{'1':0, '2':1, '3':1}}
+        print(request.client.host)  
+        return {'ports':{'1':123, '2':1, '3':1}}    # TODO: Воззвращать состояние из базы
     except Exception as ex:
         logger.error(str(ex))
         return templates.TemplateResponse("error.html", {"request": request})
@@ -35,7 +35,8 @@ async def index(request: Request):
 async def check(request: Request):
     try:
         status_json = {}
-        status_json[request.client.host] = esp.check()
+        statuses = esp.check()
+        status_json[request.client.host] = json.loads(statuses)
         with open("test.txt", "a") as file:
             file.write(str(status_json)+'\n')
         return status_json
@@ -55,8 +56,7 @@ async def hardware(request: Request):
 @esp8266.post("/hardware")
 async def hardware(request: Request, url: str = Form(...)):
     try:
-        final_url = urlCheckerObject.get_url()
-        return templates.TemplateResponse("watch.html", {"request": request, "url" : final_url})
+        return 'Building...'
     except Exception as ex:
         logger.error(str(ex))
         return templates.TemplateResponse("error.html", {"request": request})
--- esp/esp/esp.ino
+++ esp/esp/esp.ino
@@ -4,7 +4,20 @@
 #include <Arduino_JSON.h>
 
 
-const int DIN_PIN = 7;
+const int pin_1 = 1;
+const int pin_2 = 2;
+const int pin_3 = 3;
+const int pin_4 = 4;
+const int pin_7 = 7;
+const int pin_8 = 8;
+const int pin_9 = 9;
+const int pin_10 = 10;
+const int pin_11 = 11;
+const int pin_13 = 13;
+const int pin_14 = 14;
+const int pin_15 = 15;
+const int pin_16 = 16;
+
 
 void setup () {
   Serial.begin(115200);
@@ -24,15 +37,28 @@ void loop() {
     http.begin(client, "http://192.168.1.6:8090/"); 
     int httpCode = http.GET(); 
     if (httpCode > 0) {
-      String payload = http.getString();  
-      deserializeJson(doc, payload);
+      String payload = http.getString();
+      //deserializeJson(doc, payload);
+      doc["1"] = digitalRead(pin_1);
+      doc["2"] = digitalRead(pin_2);
+      doc["3"] = digitalRead(pin_3);
+      doc["4"] = digitalRead(pin_4);
+      doc["5"] = "-";
+      doc["6"] = "-";
+      doc["7"] = digitalRead(pin_7);
+      doc["8"] = digitalRead(pin_8);
+      doc["9"] = digitalRead(pin_9);
+      doc["10"] = digitalRead(pin_10);
+      doc["11"] = digitalRead(pin_11);
+      doc["12"] = "-";
+      doc["13"] = digitalRead(pin_13);
+      doc["14"] = digitalRead(pin_14);
+      doc["15"] = digitalRead(pin_15);
+      doc["16"] = digitalRead(pin_16);
+        
       String output;
       serializeJson(doc, output); 
-      //Serial.println(output);
-      //Serial.write(output);
-      int value;
-      value = digitalRead( DIN_PIN );
-      Serial.println( value );  
+      Serial.println(output); 
     }else Serial.println("An error ocurred");
     http.end();  
   }
